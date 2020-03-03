0eec8757c23e19532498243efd28ff5c0abc3587
nemanjamakuljevic <56513735+nemanjamakuljevic@users.noreply.github.com>
2020-03-03 18:46:22 +0100

    Add files via upload

---
+++ Small projects/login form anime/js/main.js
@@ -0,0 +1,18 @@
+const inputs = document.querySelectorAll('.input');
+
+function focusF(){
+    let parent = this.parentNode.parentNode;
+    parent.classList.add('focus');
+}
+
+function blurF(){
+    let parent = this.parentNode.parentNode;
+    if(this.value == ""){
+    parent.classList.remove('focus');
+    }
+}
+
+inputs.forEach(input => {
+    input.addEventListener('focus',focusF);
+    input.addEventListener('blur',blurF);    
+});
